1b593c501df8af0141f97bff93d13355e18aa5b7
Mohammad Fahrul Alwan <fahrulalwan@gmail.com>
2022-07-25 05:10:12 +0700

    add cors middleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from config.database import SessionLocal, engine
 from router.api import api_router
 from router.brands import model as brands_model
@@ -28,11 +29,24 @@ contact_metadata = {
     "email": "fahrulalwan@gmail.com"
 }
 
+
 app = FastAPI(
     title='UCARS Backend',
     description=description,
     version='1.0.0',
     openapi_tags=tags_metadata
+)
+
+origins = [
+    "http://localhost",
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
